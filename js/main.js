5d9f8794aee1a3b4e9d72a732f8a553a7b88ec56
victorbruce <victorbruce82@gmail.com>
2019-12-29 06:47:49 +0000

    Add script to randomly generate quotes

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let quotes = [
+  {
+    "quote": "I am who I am",
+    "author": "God"
+  },
+  {
+    "quote": "Hardwork beats talent if talent fails to beat hardwork",
+    "author": "Kevin Durant"
+  },
+  {
+    "quote": "A man without vision cannot accomplish his mission",
+    "author": "Victor Bruce"
+  },
+  {
+    "quote": "Hello world",
+    "author": "Programming"
+  }
+]
+
+// variables
+const btn = document.getElementById('btn');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+
+// event listeners
+btn.addEventListener('click', getQuote);
+
+function getQuote() {
+  let number = Math.floor(Math.random()  * quotes.length); 
+  quote.innerHTML = `<span>"</span>${quotes[number].quote}<span>"</span>`;
+  author.innerHTML = `<span>--</span>${quotes[number].author}`;
+} 
